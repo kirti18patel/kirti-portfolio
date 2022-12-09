09c6209a4c267a8858a695c4bf434890aa78c61b
Kirti Patel <74840827+kirti18patel@users.noreply.github.com>
2022-12-08 23:35:24 -0500

    Update index.js

--- src/components/FloatingDiv/index.js
+++ src/components/FloatingDiv/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import './FloatingDiv.css'
 
-const FloatinDiv = ({img, text1, text2}) => {
+const FloatinDiv = ({img, text1}) => {
   return (
     <div className="floatingDiv">
       <img src={img} alt="" />
       <span>
         {text1}
-        <br/>
-        {text2}
       </span>
     </div>
   );
